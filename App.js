2d964650108ab263d367608c896f1ee1dd64d38b
[hwrnandez] <[20220215@ricaldone.edu.sv]>
2024-07-08 11:25:53 -0600

    codigo de llamadas de pantallas

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Importa tus componentes de pantalla aquí
+import Login from './src/screens/Login';
+import Registro from './src/screens/Registro';
+import SplashScreen from './src/screens/SplashScreen';
+import TabNavigator from './src/tabNavigator/tabNavigator';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function inicia() {
+      try {
+        // Simula una carga inicial
+        await new Promise((resolve) => setTimeout(resolve, 3000)); // 3 segundos
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    inicia();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}>
+        {appIsReady ? (
+          <>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Registro" component={Registro} />
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          </>
+        ) : (
+          <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
