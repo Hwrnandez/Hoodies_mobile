28865260dc7516b25f0570b07d1bbb811a3d192f
[hwrnandez] <[20220215@ricaldone.edu.sv]>
2024-07-14 18:30:32 -0600

    Carrito y perfil

---
+++ src/screens/Carrito.js
@@ -0,0 +1,241 @@
+//Importaciones
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
+import * as Constantes from '../../utils/constantes';
+import CarritoCard from '../components/CarritoCard/CarritoCard';
+import ModalEditarCantidad from '../components/Modales/ModalEditarCantidad';
+import { FontAwesome } from '@expo/vector-icons'; // Importar FontAwesome desde expo
+import AwesomeAlert from 'react-native-awesome-alerts';
+
+//Configuracion del encabezado de la navegacion
+const Carrito = ({ navigation }) => {
+    useEffect(() => {
+        navigation.setOptions({
+            headerTitle: 'Carrito',
+            headerTitleAlign: 'center',
+            headerTransparent: true,
+            headerStyle: {
+                backgroundColor: 'transparent',
+            },
+            headerTintColor: '#fff',
+        });
+    }, [navigation]);
+
+     // Definición de estados
+    const ip = Constantes.IP;
+    const [dataDetalleCarrito, setDataDetalleCarrito] = useState([]);
+    const [idDetalle, setIdDetalle] = useState(null);
+    const [cantidadProductoCarrito, setCantidadProductoCarrito] = useState(0);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [showProgress, setShowProgress] = useState(false);
+
+    //Funcion para optener los detalles del carrito
+    const getDetalleCarrito = useCallback(async () => {
+        try {
+            const response = await fetch(`${ip}/services/public/pedido.php?action=readDetail`, {
+                method: 'GET',
+            });
+            const data = await response.json();
+            if (data.status) {
+                setDataDetalleCarrito(data.dataset);
+                if (data.dataset.length === 0) {
+                    showAlertWithMessage('No hay detalles del carrito disponibles.');
+                }
+            } else {
+                console.log("No hay detalles del carrito disponibles");
+                showAlertWithMessage('No hay detalles del carrito disponibles.');
+            }
+        } catch (error) {
+            console.error(error, "Error desde Catch");
+            showAlertWithMessage('Ocurrió un error al obtener los detalles del carrito.');
+        }
+    }, [ip]);
+
+     // UseEffect para actualizar los detalles del carrito al enfocarse en la pantalla
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {
+            getDetalleCarrito();
+        });
+        return unsubscribe;
+    }, [navigation, getDetalleCarrito]);
+
+    //Fumncion para finalizar el pedido
+    const finalizarPedido = async () => {
+        try {
+            const response = await fetch(`${ip}/services/public/pedido.php?action=finishOrder`, {
+                method: 'GET',
+            });
+            const data = await response.json();
+            if (data.status) {
+                showAlertWithMessage('Se finalizó la compra correctamente.');
+                setDataDetalleCarrito([]);
+                navigation.reset({
+                    index: 0,
+                    routes: [{ name: 'Productos' }],
+                }); // Navegar a Productos al finalizar el pedido
+            } else {
+                showAlertWithMessage(data.error);
+            }
+        } catch (error) {
+            showAlertWithMessage('Ocurrió un error al finalizar el pedido.');
+        }
+    };
+
+    // Función para manejar la edición de detalles del carrito
+    const handleEditarDetalle = (idDetalle, cantidadDetalle) => {
+        setModalVisible(true);
+        setIdDetalle(idDetalle);
+        setCantidadProductoCarrito(cantidadDetalle);
+    };
+
+    //render de elementos del carrito
+    const renderItem = ({ item }) => (
+        <CarritoCard
+            item={item}
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+            setCantidadProductoCarrito={setCantidadProductoCarrito}
+            cantidadProductoCarrito={cantidadProductoCarrito}
+            idDetalle={idDetalle}
+            setIdDetalle={setIdDetalle}
+            accionBotonDetalle={handleEditarDetalle}
+            getDetalleCarrito={getDetalleCarrito}
+            updateDataDetalleCarrito={setDataDetalleCarrito}
+        />
+    );
+
+    //Funcion para alerta
+    const showAlertWithMessage = (message, showProgressIndicator = false) => {
+        setAlertMessage(message);
+        setShowProgress(showProgressIndicator);
+        setShowAlert(true);
+    };
+
+    //Componente principal
+    return (
+        <SafeAreaView style={styles.container}>
+            <ImageBackground source={require('../img/fondo.png')} style={styles.backgroundImage}>
+                <View style={styles.contentContainer}>
+                    <ModalEditarCantidad
+                        setModalVisible={setModalVisible}
+                        modalVisible={modalVisible}
+                        idDetalle={idDetalle}
+                        setIdDetalle={setIdDetalle}
+                        setCantidadProductoCarrito={setCantidadProductoCarrito}
+                        cantidadProductoCarrito={cantidadProductoCarrito}
+                        getDetalleCarrito={getDetalleCarrito}
+                    />
+                    {dataDetalleCarrito.length > 0 ? (
+                        <FlatList
+                            data={dataDetalleCarrito}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.id_detalle.toString()}
+                        />
+                    ) : (
+                        <View style={styles.emptyCartContainer}>
+                            <Text style={styles.titleDetalle}>No hay detalles del carrito disponibles.</Text>
+                        </View>
+                    )}
+                    {dataDetalleCarrito.length > 0 && (
+                        <TouchableOpacity style={styles.button} onPress={finalizarPedido}>
+                            <FontAwesome name="check" size={20} color="white" style={styles.buttonIcon} />
+                            <Text style={styles.buttonText}>Finalizar Pedido</Text>
+                        </TouchableOpacity>
+                    )}
+                    <AwesomeAlert
+                        show={showAlert}
+                        showProgress={showProgress}
+                        title="Alerta"
+                        message={alertMessage}
+                        closeOnTouchOutside={!showProgress}
+                        closeOnHardwareBackPress={!showProgress}
+                        showCancelButton={false}
+                        showConfirmButton={!showProgress}
+                        confirmText="OK"
+                        confirmButtonColor="gray"
+                        onConfirmPressed={() => {
+                            setShowAlert(false);
+                        }}
+                        contentContainerStyle={styles.alertContentContainer}
+                        titleStyle={styles.alertTitle}
+                        messageStyle={styles.alertMessage}
+                        confirmButtonStyle={styles.alertConfirmButton}
+                        confirmButtonTextStyle={styles.alertConfirmButtonText}
+                    />
+                </View>
+            </ImageBackground>
+        </SafeAreaView>
+    );
+};
+
+export default Carrito;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0A305E',
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover', // Ajusta la imagen para que cubra el fondo
+    },
+    contentContainer: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingTop: 100,
+        alignItems: 'center',
+    },
+    titleDetalle: {
+        fontSize: 20,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginVertical: 16,
+        color: '#fff',
+    },
+    button: {
+        backgroundColor: 'green',
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        marginBottom: 10,
+        flexDirection: 'row', // Alinear ícono y texto en una fila
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    buttonIcon: {
+        marginRight: 10, // Espacio entre el ícono y el texto
+    },
+    emptyCartContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    alertContentContainer: {
+        borderRadius: 10,
+        padding: 20,
+    },
+    alertTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    alertMessage: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    alertConfirmButton: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    alertConfirmButtonText: {
+        fontSize: 16,
+    },
+});
---
+++ src/screens/Perfil.js
@@ -0,0 +1,285 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import AwesomeAlert from 'react-native-awesome-alerts';
+import * as Constantes from '../../utils/constantes';
+
+export default function Perfil({ navigation }) {
+    // Obtener la IP de las constantes
+    const ip = Constantes.IP;
+
+    useEffect(() => {
+        // Configurar opciones de navegación
+        navigation.setOptions({
+            headerTitle: 'Perfil', // Título de la pantalla
+            headerTitleAlign: 'center', // Centrar el título
+            headerTransparent: true, // Hacer el header transparente
+            headerStyle: {
+                backgroundColor: 'transparent', // Fondo transparente
+            },
+            headerTintColor: '#fff', // Color del texto
+            headerRight: () => (
+                // Botón de cierre de sesión en la esquina superior derecha
+                <TouchableOpacity onPress={handleLogout}>
+                    <Image source={require('../img/logout.png')} style={styles.logoutImage} />
+                </TouchableOpacity>
+            ),
+        });
+    }, []);
+    // Estado para manejar la actualización de datos
+    const [refreshing, setRefreshing] = useState(false);
+
+    // Estado para almacenar los datos del perfil del usuario
+    const [profileData, setProfileData] = useState({
+        nombre_cliente: '',
+        apellido_cliente: '',
+        direccion_cliente: '',
+        telefono_cliente: '',
+        correo_cliente: ''
+    });
+    // Estado para manejar la visibilidad y el mensaje de la alerta
+    const [alertVisible, setAlertVisible] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertType, setAlertType] = useState('success');
+
+    // Función para obtener los datos del perfil desde el servidor
+    const fetchProfileData = async () => {
+        try {
+            const response = await fetch(`${ip}/services/public/clientes.php?action=readProfile`, {
+                method: 'POST' // Método POST para la solicitud
+            });
+            const data = await response.json(); // Convertir la respuesta a JSON
+            if (data.status) {
+                setProfileData(data.dataset); // Actualizar el estado con los datos del perfil
+            } else {
+                showAlert('Error', 'Failed to fetch profile data', 'error');
+            }
+        } catch (error) {
+            console.error(error, "Error desde Catch");
+            showAlert('Error', 'Ocurrió un error al obtener los datos del perfil', 'error');
+        }
+    };
+
+    // Función para manejar el cierre de sesión del usuario
+    const handleLogout = async () => {
+        try {
+            const response = await fetch(`${ip}/services/public/clientes.php?action=logOut`, {
+                method: 'GET'
+            });
+            const data = await response.json(); // Convertir la respuesta a JSON
+            if (data.status) {
+                showAlert('Éxito', "Has cerrado sesión exitosamente.", 'success');
+                setTimeout(() => {
+                    setAlertVisible(false);  // Ocultar la alerta después de un tiempo
+                    navigation.navigate('Login'); // Navegar a la pantalla de inicio de sesión
+                }, 2000);
+            } else {
+                showAlert('Error', "Error al cerrar sesión.", 'error');
+            }
+        } catch (error) {
+            showAlert('Error de red', "Ocurrió un error de red.", 'error');
+        }
+    };
+
+    // Función para editar los datos del perfil del usuario
+    const editProfile = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('nombreCliente', profileData.nombre_cliente);
+            formData.append('apellidoCliente', profileData.apellido_cliente);
+            formData.append('direccionCliente', profileData.direccion_cliente);
+            formData.append('telefonoCliente', profileData.telefono_cliente);
+            formData.append('correoCliente', profileData.correo_cliente);
+
+            const response = await fetch(`${ip}/services/public/clientes.php?action=editProfile`, {
+                method: 'POST', // Método POST para la solicitud
+                body: formData, // Cuerpo de la solicitud con los datos del formulario
+            });
+            const data = await response.json();
+            if (data.status) {
+                showAlert('Success', data.message, 'success');
+            } else {
+                showAlert('Error', data.error, 'error');
+            }
+        } catch (error) {
+            console.error('Error :', error);
+            showAlert('Error', 'Error al registrar', 'error');
+        }
+    };
+    // Función para mostrar una alerta con un mensaje específico
+    const showAlert = (title, message, type) => {
+        setAlertMessage(message); // Establecer el mensaje de la alerta
+        setAlertType(type); // Establecer el tipo de alerta
+        setAlertVisible(true); // Mostrar la alerta
+    };
+
+    useEffect(() => {
+        // Obtener los datos del perfil al cargar la pantalla
+        fetchProfileData();
+    }, []);
+    // Función para refrescar los datos del perfil
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        fetchProfileData().then(() => setRefreshing(false));
+    }, []);
+
+    return (
+        <ImageBackground source={require('../img/fondo.png')} style={styles.backgroundImage}>
+            <ScrollView contentContainerStyle={styles.container}>
+                <View style={styles.form}>
+                    <Text style={styles.sectionTitle}>Mi información</Text>
+                    <Text style={styles.label}>Nombre</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={profileData.nombre_cliente}
+                        onChangeText={(text) => setProfileData((prevData) => ({ ...prevData, nombre_cliente: text }))}
+                        placeholder="Nombre"
+                        placeholderTextColor="#fff"
+                    />
+                    <Text style={styles.label}>Apellido</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={profileData.apellido_cliente}
+                        onChangeText={(text) => setProfileData((prevData) => ({ ...prevData, apellido_cliente: text }))}
+                        placeholder="Apellido"
+                        placeholderTextColor="#fff"
+                    />
+                    <Text style={styles.label}>Dirección</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={profileData.direccion_cliente}
+                        onChangeText={(text) => setProfileData((prevData) => ({ ...prevData, direccion_cliente: text }))}
+                        placeholder="Dirección"
+                        placeholderTextColor="#fff"
+                    />
+                    <Text style={styles.label}>Teléfono</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={profileData.telefono_cliente}
+                        onChangeText={(text) => setProfileData((prevData) => ({ ...prevData, telefono_cliente: text }))}
+                        placeholder="Teléfono"
+                        placeholderTextColor="#fff"
+                        keyboardType="phone-pad"
+                    />
+                    <Text style={styles.label}>Correo</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={profileData.correo_cliente}
+                        onChangeText={(text) => setProfileData((prevData) => ({ ...prevData, correo_cliente: text }))}
+                        placeholder="Correo"
+                        placeholderTextColor="#fff"
+                        keyboardType="email-address"
+                    />
+                </View>
+                <TouchableOpacity style={styles.button} onPress={editProfile}>
+                    <Text style={styles.buttonText}>Guardar</Text>
+                </TouchableOpacity>
+            </ScrollView>
+            <AwesomeAlert
+                show={alertVisible}
+                showProgress={false}
+                title={alertType === 'success' ? 'Éxito' : 'Error'}
+                message={alertMessage}
+                closeOnTouchOutside={true}
+                closeOnHardwareBackPress={false}
+                showConfirmButton={true}
+                confirmText="OK"
+                confirmButtonColor={alertType === 'success' ? '#0A305E' : '#DD6B55'}
+                confirmButtonStyle={styles.alertConfirmButton}
+                confirmButtonTextStyle={styles.alertConfirmButtonText}
+                onConfirmPressed={() => setAlertVisible(false)}
+                contentContainerStyle={styles.alertContentContainer}
+                titleStyle={styles.alertTitle}
+                messageStyle={styles.alertMessage}
+            />
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#0A305E',
+    },
+    container: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+        paddingTop: 50,
+    },
+    form: {
+        marginBottom: 30,
+        width: '100%',
+    },
+    sectionTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 10,
+        textAlign: 'center',
+        marginTop: 50,
+    },
+    label: {
+        fontSize: 16,
+        color: '#fff',
+        marginBottom: 5,
+    },
+    input: {
+        width: '100%',
+        height: 50,
+        borderColor: '#fff',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingLeft: 10,
+        marginBottom: 20,
+        fontSize: 18, // Ajuste el tamaño de fuente a 18
+        color: '#fff',
+    },
+    button: {
+        width: '50%',
+        height: 50,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#0A305E',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    logoutImage: {
+        width: 25,
+        height: 25,
+        marginRight: 10,
+    },
+    confirmButton: {
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+    },
+    confirmButtonText: {
+        fontSize: 18,
+    },
+    alertContentContainer: {
+        borderRadius: 10,
+        padding: 20,
+    },
+    alertTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    alertMessage: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    alertConfirmButton: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    alertConfirmButtonText: {
+        fontSize: 16,
+    },
+});
